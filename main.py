211ac78b0ce8a267ef9b77f881412ba87140d39b
Habdio <87224211+Habdio@users.noreply.github.com>
2022-03-18 19:19:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyrogram import Client
 
 PyrogramBot = Client(
     "PyrogramBot",
-    api_hash="d9647913e97bf2f6a66d978290284028",
-    api_id="17946666",
-    bot_token="5125014420:AAEK2E9aj-q9FHZknW_hrisJvCuONx0XPtU",
+    api_hash="09ca4ef17b06c5030d4e8f7cbd92f1a9",
+    api_id="10342078",
+    bot_token="5206875671:AAF1Z_n7z6DEB-0KOUxL_d6EZ3N29jCToyg",
     plugins=dict(root="PyrogramBot")
   )
 
